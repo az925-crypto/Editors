9a2470093e4578168f374f1644d0d1d81cd1b522
az925-crypto <azzamalfarisy925@gmail.com>
2026-08-25 04:23:01 +0000

    Fix: simplify Shape/Theme to minimal compile-ready

--- app/src/main/java/com/zaaam/editors/ui/theme/Shape.kt
+++ app/src/main/java/com/zaaam/editors/ui/theme/Shape.kt
@@ -1,8 +1,6 @@
 package com.zaaam.editors.ui.theme
 
-import androidx.compose.ui.graphics.CornerSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 object RetroShapes {
     val Shell = 22.dp
@@ -15,15 +13,4 @@ object RetroShapes {
     val Pill = 999.dp
     val Stencil = 10.dp
     val NavItem = 12.dp
-
-    val ShellCorner = CornerSize(Shell)
-    val HeroCardCorner = CornerSize(HeroCard)
-    val CardCorner = CornerSize(Card)
-    val SearchBtnCorner = CornerSize(SearchBtn)
-    val DialogCorner = CornerSize(Dialog)
-    val TabTopCorner = CornerSize(TabTop)
-    val ChipCorner = CornerSize(Chip)
-    val PillCorner = CornerSize(Pill)
-    val StencilCorner = CornerSize(Stencil)
-    val NavItemCorner = CornerSize(NavItem)
 }
